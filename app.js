98aedd9f017eb7825f55ef188000a0ff96dec22c
b00902130 <b00902130@ntu.edu.tw>
2014-06-02 11:30:56 +0800

    Change view engine from jade to ejs.

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var path = require('path');
+var http = require('http');
 var favicon = require('static-favicon');
+var portNum = process.env.PORT || 3000;
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index');
 
 var app = express();
 
 // view engine setup
+app.set('port', portNum);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -21,8 +23,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', index.load);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
@@ -55,6 +56,9 @@ app.use(function(err, req, res, next) {
     });
 });
 
-app.listen(3000);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+// app.listen(3000);
 
-module.exports = app;
+// module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
-var router = express.Router();
+// var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res) {
+//   res.render('index', { title: 'Express' });
+// });
 
-module.exports = router;
+exports.load = function(req, res){
+	res.render('index', {'title': 'CourseGom!', 'anotherGom': 'gomgom7745'});
+}
+// module.exports = router;
